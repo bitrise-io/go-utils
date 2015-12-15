68ff0a5ddbc0e537f134f1b25471a0715ec1254e
Krisztián Gödrei <godrekrisztian@gmil.com>
2015-12-15 11:06:11 +0100

    cast to ptr

--- parseutil/parseutil_test.go
+++ parseutil/parseutil_test.go
@@ -31,3 +31,65 @@ func TestCastToString(t *testing.T) {
 	require.Equal(t, "true", CastToString(true))
 	require.Equal(t, "false", CastToString("false"))
 }
+
+func TestCastToStringPtr(t *testing.T) {
+	require.Equal(t, "1", *CastToStringPtr(1))
+	require.Equal(t, "0.1", *CastToStringPtr(0.1))
+	require.Equal(t, "true", *CastToStringPtr(true))
+	require.Equal(t, "false", *CastToStringPtr(false))
+	require.Equal(t, "yes", *CastToStringPtr("yes"))
+}
+
+func TestCastToBoolPtr(t *testing.T) {
+	casted, ok := CastToBoolPtr(true)
+	require.Equal(t, true, ok)
+	require.Equal(t, true, *casted)
+
+	casted, ok = CastToBoolPtr("true")
+	require.Equal(t, true, ok)
+	require.Equal(t, true, *casted)
+
+	casted, ok = CastToBoolPtr(false)
+	require.Equal(t, true, ok)
+	require.Equal(t, false, *casted)
+
+	casted, ok = CastToBoolPtr("false")
+	require.Equal(t, true, ok)
+	require.Equal(t, false, *casted)
+
+	casted, ok = CastToBoolPtr("yes")
+	require.Equal(t, true, ok)
+	require.Equal(t, true, *casted)
+
+	casted, ok = CastToBoolPtr("no")
+	require.Equal(t, true, ok)
+	require.Equal(t, false, *casted)
+
+	casted, ok = CastToBoolPtr(1)
+	require.Equal(t, true, ok)
+	require.Equal(t, true, *casted)
+
+	casted, ok = CastToBoolPtr("1")
+	require.Equal(t, true, ok)
+	require.Equal(t, true, *casted)
+
+	casted, ok = CastToBoolPtr(0)
+	require.Equal(t, true, ok)
+	require.Equal(t, false, *casted)
+
+	casted, ok = CastToBoolPtr("0")
+	require.Equal(t, true, ok)
+	require.Equal(t, false, *casted)
+
+	casted, ok = CastToBoolPtr(0.1)
+	require.Equal(t, false, ok)
+	require.Equal(t, (*bool)(nil), casted)
+
+	casted, ok = CastToBoolPtr("0.1")
+	require.Equal(t, false, ok)
+	require.Equal(t, (*bool)(nil), casted)
+
+	casted, ok = CastToBoolPtr("test")
+	require.Equal(t, false, ok)
+	require.Equal(t, (*bool)(nil), casted)
+}

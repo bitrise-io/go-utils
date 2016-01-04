3dd9dfcb82bf94488160b3ce2ca2a4a5b5aa243f
Viktor Benei <viktor.benei@gmail.com>
2016-01-04 19:56:32 +0100

    sliceutil : IndexOfStringInSlice

---
+++ sliceutil/sliceutil.go
@@ -0,0 +1,11 @@
+package pointers
+
+// IndexOfStringInSlice ...
+func IndexOfStringInSlice(searchFor string, searchIn []string) int {
+	for idx, anItm := range searchIn {
+		if anItm == searchFor {
+			return idx
+		}
+	}
+	return -1
+}
---
+++ sliceutil/sliceutil_test.go
@@ -0,0 +1,25 @@
+package pointers
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestIndexOfStringInSlice(t *testing.T) {
+	t.Log("Empty slice")
+	require.Equal(t, -1, IndexOfStringInSlice("abc", []string{}))
+
+	testSlice := []string{"abc", "def", "123", "456", "123"}
+
+	t.Log("Find item")
+	require.Equal(t, 0, IndexOfStringInSlice("abc", testSlice))
+	require.Equal(t, 1, IndexOfStringInSlice("def", testSlice))
+	require.Equal(t, 3, IndexOfStringInSlice("456", testSlice))
+
+	t.Log("Find first item, if multiple")
+	require.Equal(t, 2, IndexOfStringInSlice("123", testSlice))
+
+	t.Log("Item is not in the slice")
+	require.Equal(t, -1, IndexOfStringInSlice("cba", testSlice))
+}

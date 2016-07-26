4d9e2fe0279b71d45708513fa6a7eae7b6ea1dca
Krisztián Gödrei <godrekrisztian@gmil.com>
2016-07-26 13:01:03 +0200

    removed deprecation

--- cmdex/run.go
+++ cmdex/run.go
@@ -2,7 +2,6 @@ package cmdex
 
 import (
 	"errors"
-	"fmt"
 	"io"
 	"os"
 	"os/exec"
@@ -15,11 +14,7 @@ import (
 
 const (
 	systemRubyPth = "/usr/bin/ruby"
-	brewRubyPth   = "/usr/local/bin/ruby"
 )
-
-// DeveloperModeEnvKey ...
-const DeveloperModeEnvKey = "DEVELOPER_MODE"
 
 // CommandModel ...
 type CommandModel struct {
@@ -143,20 +138,8 @@ func PrintableCommandArgs(isQuoteFirst bool, fullCommandArgs []string) string {
 	return strings.Join(cmdArgsDecorated, " ")
 }
 
-// Replacement - RunCommandWithReaderAndWriters ...
-func runCommandWithReaderAndWriters(inReader io.Reader, outWriter, errWriter io.Writer, name string, args ...string) error {
-	return NewCommand(name, args...).Stdin(inReader).Stdout(outWriter).Stderr(errWriter).Run()
-}
-
 // RunCommandWithReaderAndWriters ...
 func RunCommandWithReaderAndWriters(inReader io.Reader, outWriter, errWriter io.Writer, name string, args ...string) error {
-	if os.Getenv(DeveloperModeEnvKey) == "1" {
-		deprecationMsg := `DEPRECATED METHOD! - RunCommandWithReaderAndWriters(inReader io.Reader, outWriter, errWriter io.Writer, name string, args ...string) error
-USE INSTEAD! - NewCommand(name, args...).Stdin(inReader).Stdout(outWriter).Stderr(errWriter).Run()
-`
-		fmt.Println(deprecationMsg)
-	}
-
 	cmd := exec.Command(name, args...)
 	cmd.Stdin = inReader
 	cmd.Stdout = outWriter
@@ -164,40 +147,16 @@ USE INSTEAD! - NewCommand(name, args...).Stdin(inReader).Stdout(outWriter).Stder
 	return cmd.Run()
 }
 
-// Replacement - RunCommandWithWriters ...
-func runCommandWithWriters(outWriter, errWriter io.Writer, name string, args ...string) error {
-	return NewCommand(name, args...).Stdout(outWriter).Stderr(errWriter).Run()
-}
-
 // RunCommandWithWriters ...
 func RunCommandWithWriters(outWriter, errWriter io.Writer, name string, args ...string) error {
-	if os.Getenv(DeveloperModeEnvKey) == "1" {
-		deprecationMsg := `DEPRECATED METHOD! - RunCommandWithWriters(outWriter, errWriter io.Writer, name string, args ...string) error
-USE INSTEAD! - NewCommand(name, args...).Stdout(outWriter).Stderr(errWriter).Run()
-`
-		fmt.Println(deprecationMsg)
-	}
-
 	cmd := exec.Command(name, args...)
 	cmd.Stdout = outWriter
 	cmd.Stderr = errWriter
 	return cmd.Run()
 }
 
-// Replacement - RunCommandInDirWithEnvsAndReturnExitCode ...
-func runCommandInDirWithEnvsAndReturnExitCode(envs []string, dir, name string, args ...string) (int, error) {
-	return NewCommand(name, args...).Envs(envs).Dir(dir).RunForExitCode()
-}
-
 // RunCommandInDirWithEnvsAndReturnExitCode ...
 func RunCommandInDirWithEnvsAndReturnExitCode(envs []string, dir, name string, args ...string) (int, error) {
-	if os.Getenv(DeveloperModeEnvKey) == "1" {
-		deprecationMsg := `DEPRECATED METHOD! - RunCommandInDirWithEnvsAndReturnExitCode(envs []string, dir, name string, args ...string) (int, error)
-USE INSTEAD! - NewCommand(name, args...).Envs(envs).Dir(dir).RunForExitCode()
-`
-		fmt.Println(deprecationMsg)
-	}
-
 	cmd := exec.Command(name, args...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
@@ -224,54 +183,18 @@ USE INSTEAD! - NewCommand(name, args...).Envs(envs).Dir(dir).RunForExitCode()
 	return 0, nil
 }
 
-// Replacement - RunCommandInDirAndReturnExitCode ...
-func runCommandInDirAndReturnExitCode(dir, name string, args ...string) (int, error) {
-	return NewCommand(name, args...).Dir(dir).RunForExitCode()
-}
-
 // RunCommandInDirAndReturnExitCode ...
 func RunCommandInDirAndReturnExitCode(dir, name string, args ...string) (int, error) {
-	if os.Getenv(DeveloperModeEnvKey) == "1" {
-		deprecationMsg := `DEPRECATED METHOD! - RunCommandInDirAndReturnExitCode(dir, name string, args ...string) (int, error)
-USE INSTEAD! - NewCommand(name, args...).Dir(dir).RunForExitCode()
-`
-		fmt.Println(deprecationMsg)
-	}
-
 	return RunCommandInDirWithEnvsAndReturnExitCode([]string{}, dir, name, args...)
-}
-
-// Replacement - runCommandWithEnvsAndReturnExitCode ...
-func runCommandWithEnvsAndReturnExitCode(envs []string, name string, args ...string) (int, error) {
-	return NewCommand(name, args...).Envs(envs).RunForExitCode()
 }
 
 // RunCommandWithEnvsAndReturnExitCode ...
 func RunCommandWithEnvsAndReturnExitCode(envs []string, name string, args ...string) (int, error) {
-	if os.Getenv(DeveloperModeEnvKey) == "1" {
-		deprecationMsg := `DEPRECATED METHOD! - RunCommandWithEnvsAndReturnExitCode(envs []string, name string, args ...string) (int, error)
-USE INSTEAD! - NewCommand(name, args...).Envs(envs).RunForExitCode()
-`
-		fmt.Println(deprecationMsg)
-	}
-
 	return RunCommandInDirWithEnvsAndReturnExitCode(envs, "", name, args...)
-}
-
-// Replacement - RunCommandInDir ...
-func runCommandInDir(dir, name string, args ...string) error {
-	return NewCommand(name, args...).Dir(dir).Run()
 }
 
 // RunCommandInDir ...
 func RunCommandInDir(dir, name string, args ...string) error {
-	if os.Getenv(DeveloperModeEnvKey) == "1" {
-		deprecationMsg := `DEPRECATED METHOD! - RunCommandInDir(dir, name string, args ...string) error
-USE INSTEAD! - NewCommand(name, args...).Dir(dir).Run()
-`
-		fmt.Println(deprecationMsg)
-	}
-
 	cmd := exec.Command(name, args...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
@@ -282,57 +205,21 @@ USE INSTEAD! - NewCommand(name, args...).Dir(dir).Run()
 	return cmd.Run()
 }
 
-// Replacement - runCommand ...
-func runCommand(name string, args ...string) error {
-	return NewCommand(name, args...).Run()
-}
-
 // RunCommand ...
 func RunCommand(name string, args ...string) error {
-	if os.Getenv(DeveloperModeEnvKey) == "1" {
-		deprecationMsg := `DEPRECATED METHOD! - RunCommand(name string, args ...string) error
-USE INSTEAD! - NewCommand(name, args...).Run()
-`
-		fmt.Println(deprecationMsg)
-	}
-
 	return RunCommandInDir("", name, args...)
-}
-
-// Replacement - RunCommandAndReturnStdout ...
-func runCommandAndReturnStdout(name string, args ...string) (string, error) {
-	return NewCommand(name, args...).RunForOutput()
 }
 
 // RunCommandAndReturnStdout ..
 func RunCommandAndReturnStdout(name string, args ...string) (string, error) {
-	if os.Getenv(DeveloperModeEnvKey) == "1" {
-		deprecationMsg := `DEPRECATED METHOD! - RunCommandAndReturnStdout(name string, args ...string) (string, error)
-USE INSTEAD! - NewCommand(name, args...).RunForOutput()
-`
-		fmt.Println(deprecationMsg)
-	}
-
 	cmd := exec.Command(name, args...)
 	outBytes, err := cmd.Output()
 	outStr := string(outBytes)
 	return strings.TrimSpace(outStr), err
 }
 
-// Replacement - runCommandInDirAndReturnCombinedStdoutAndStderr ...
-func runCommandInDirAndReturnCombinedStdoutAndStderr(dir, name string, args ...string) (string, error) {
-	return NewCommand(name, args...).Dir(dir).RunForCombinedOutput()
-}
-
 // RunCommandInDirAndReturnCombinedStdoutAndStderr ...
 func RunCommandInDirAndReturnCombinedStdoutAndStderr(dir, name string, args ...string) (string, error) {
-	if os.Getenv(DeveloperModeEnvKey) == "1" {
-		deprecationMsg := `DEPRECATED METHOD! - RunCommandInDirAndReturnCombinedStdoutAndStderr(dir, name string, args ...string) (string, error)
-USE INSTEAD! - NewCommand(name, args...).Dir(dir).RunForCombinedOutput()
-`
-		fmt.Println(deprecationMsg)
-	}
-
 	cmd := exec.Command(name, args...)
 	if dir != "" {
 		cmd.Dir = dir
@@ -342,65 +229,18 @@ USE INSTEAD! - NewCommand(name, args...).Dir(dir).RunForCombinedOutput()
 	return strings.TrimSpace(outStr), err
 }
 
-// Replacement - RunCommandAndReturnCombinedStdoutAndStderr ...
-func runCommandAndReturnCombinedStdoutAndStderr(name string, args ...string) (string, error) {
-	return NewCommand(name, args...).RunForCombinedOutput()
-}
-
 // RunCommandAndReturnCombinedStdoutAndStderr ..
 func RunCommandAndReturnCombinedStdoutAndStderr(name string, args ...string) (string, error) {
-	if os.Getenv(DeveloperModeEnvKey) == "1" {
-		deprecationMsg := `DEPRECATED METHOD! - RunCommandAndReturnCombinedStdoutAndStderr(name string, args ...string) (string, error)
-USE INSTEAD! - NewCommand(name, args...).RunForCombinedOutput()
-`
-		fmt.Println(deprecationMsg)
-	}
-
 	return RunCommandInDirAndReturnCombinedStdoutAndStderr("", name, args...)
-}
-
-// Replacement - runBashCommand ...
-func runBashCommand(cmdStr string) error {
-	return NewBashCommand(cmdStr).Run()
-}
-
-// Or mutch more better
-// Replacement - runBashCommand ...
-func betterRunBashCommand(name string, args ...string) error {
-	return NewBashCommand(name, args...).Run()
 }
 
 // RunBashCommand ...
 func RunBashCommand(cmdStr string) error {
-	if os.Getenv(DeveloperModeEnvKey) == "1" {
-		deprecationMsg := `DEPRECATED METHOD! - RunBashCommand(cmdStr string) error
-USE INSTEAD! - NewBashCommand(name, args...).Run()
-`
-		fmt.Println(deprecationMsg)
-	}
-
 	return RunCommand("bash", "-c", cmdStr)
-}
-
-// Replacement - runBashCommandLines ...
-func runBashCommandLines(cmdLines []string) error {
-	for _, aLine := range cmdLines {
-		if err := NewBashCommand(aLine).Run(); err != nil {
-			return err
-		}
-	}
-	return nil
 }
 
 // RunBashCommandLines ...
 func RunBashCommandLines(cmdLines []string) error {
-	if os.Getenv(DeveloperModeEnvKey) == "1" {
-		deprecationMsg := `DEPRECATED METHOD! - RunBashCommandLines(cmdLines []string) error
-USE INSTEAD! - NewBashCommand(aLine).Run()
-`
-		fmt.Println(deprecationMsg)
-	}
-
 	for _, aLine := range cmdLines {
 		if err := RunCommand("bash", "-c", aLine); err != nil {
 			return err

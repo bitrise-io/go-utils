a932f484452fef958d669a1f39a53fce18483ec4
Krisztián Gödrei <godrekrisztian@gmil.com>
2016-08-08 12:36:56 +0200

    TestNewCommandSlice

--- cmdex/run.go
+++ cmdex/run.go
@@ -31,14 +31,10 @@ func NewCommandFromSlice(cmdSlice []string) (*CommandModel, error) {
 	if len(cmdSlice) == 0 {
 		return nil, errors.New("no command provided")
 	} else if len(cmdSlice) == 1 {
-		return &CommandModel{
-			cmd: exec.Command(cmdSlice[0]),
-		}, nil
+		return NewCommand(cmdSlice[0]), nil
 	}
 
-	return &CommandModel{
-		cmd: exec.Command(cmdSlice[0], cmdSlice[1:]...),
-	}, nil
+	return NewCommand(cmdSlice[0], cmdSlice[1:]...), nil
 }
 
 // NewCommandWithCmd ...

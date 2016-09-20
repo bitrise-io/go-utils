e859c0f9b0cea4ba3fe7efebd4ee99176d2ee490
Viktor Benei <viktor.benei@gmail.com>
2016-09-20 18:15:16 +0200

    stringutil - KeysOfStringInterfaceMap

--- maputil/maputil.go
+++ maputil/maputil.go
@@ -1,5 +1,18 @@
 package maputil
 
+// KeysOfStringInterfaceMap ...
+func KeysOfStringInterfaceMap(m map[string]interface{}) []string {
+	keys := make([]string, len(m))
+
+	i := 0
+	for k := range m {
+		keys[i] = k
+		i++
+	}
+
+	return keys
+}
+
 // KeysOfStringStringMap ...
 func KeysOfStringStringMap(m map[string]string) []string {
 	keys := make([]string, len(m))
--- maputil/maputil_test.go
+++ maputil/maputil_test.go
@@ -7,21 +7,74 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestKeysOfStringStringMap(t *testing.T) {
+func TestKeysOfStringInterfaceMap(t *testing.T) {
 	t.Log("Empty map")
-	keys := KeysOfStringStringMap(map[string]string{})
-	require.Equal(t, []string{}, keys)
-	require.Equal(t, 0, len(keys))
+	{
+		keys := KeysOfStringInterfaceMap(map[string]interface{}{})
+		require.Equal(t, []string{}, keys)
+		require.Equal(t, 0, len(keys))
+	}
+
+	t.Log("Nil map")
+	{
+		keys := KeysOfStringInterfaceMap(map[string]interface{}(nil))
+		require.Equal(t, []string{}, keys)
+		require.Equal(t, 0, len(keys))
+
+		var nilMap map[string]interface{}
+		keys = KeysOfStringInterfaceMap(nilMap)
+		require.Equal(t, []string{}, keys)
+		require.Equal(t, 0, len(keys))
+	}
 
 	t.Log("Single key")
-	keys = KeysOfStringStringMap(map[string]string{"a": "value"})
-	require.Equal(t, 1, len(keys))
-	require.Equal(t, []string{"a"}, keys)
+	{
+		keys := KeysOfStringInterfaceMap(map[string]interface{}{"a": "value"})
+		require.Equal(t, 1, len(keys))
+		require.Equal(t, []string{"a"}, keys)
+	}
 
 	t.Log("Multiple keys")
-	keys = KeysOfStringStringMap(map[string]string{"a": "value 1", "b": "value 2"})
-	require.Equal(t, 2, len(keys))
-	testutil.EqualSlicesWithoutOrder(t, []string{"a", "b"}, keys)
+	{
+		keys := KeysOfStringInterfaceMap(map[string]interface{}{"a": "value 1", "b": "value 2"})
+		require.Equal(t, 2, len(keys))
+		testutil.EqualSlicesWithoutOrder(t, []string{"a", "b"}, keys)
+	}
+}
+
+func TestKeysOfStringStringMap(t *testing.T) {
+	t.Log("Empty map")
+	{
+		keys := KeysOfStringStringMap(map[string]string{})
+		require.Equal(t, []string{}, keys)
+		require.Equal(t, 0, len(keys))
+	}
+
+	t.Log("Nil map")
+	{
+		keys := KeysOfStringStringMap(map[string]string(nil))
+		require.Equal(t, []string{}, keys)
+		require.Equal(t, 0, len(keys))
+
+		var nilMap map[string]string
+		keys = KeysOfStringStringMap(nilMap)
+		require.Equal(t, []string{}, keys)
+		require.Equal(t, 0, len(keys))
+	}
+
+	t.Log("Single key")
+	{
+		keys := KeysOfStringStringMap(map[string]string{"a": "value"})
+		require.Equal(t, 1, len(keys))
+		require.Equal(t, []string{"a"}, keys)
+	}
+
+	t.Log("Multiple keys")
+	{
+		keys := KeysOfStringStringMap(map[string]string{"a": "value 1", "b": "value 2"})
+		require.Equal(t, 2, len(keys))
+		testutil.EqualSlicesWithoutOrder(t, []string{"a", "b"}, keys)
+	}
 }
 
 func TestCloneStringStringMap(t *testing.T) {

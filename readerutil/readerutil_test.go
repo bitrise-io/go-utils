04ba16d19fbae012aeff4890bb080b21df91df46
Viktor Benei <viktor.benei@gmail.com>
2016-10-05 21:57:50 +0200

    readerutil.WalkLines (#37)
    a wrapper around readerutil.ReadLongLine, for easier usage

--- readerutil/readerutil_test.go
+++ readerutil/readerutil_test.go
@@ -2,6 +2,7 @@ package readerutil
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"io"
 	"strings"
@@ -73,5 +74,109 @@ third, really long line: %s
 		// check the start of the long line
 		require.Equal(t, "third, really long line: ---", lines[2][0:28])
 		require.Equal(t, "  fourth line", lines[3])
+	}
+}
+
+func TestWalkLines(t *testing.T) {
+	t.Log("Empty string")
+	{
+		inputStr := ``
+		//
+		lines := []string{}
+		err := WalkLines(inputStr, func(line string) error {
+			lines = append(lines, line)
+			return nil
+		})
+		require.Equal(t, nil, err)
+		require.Equal(t, []string{}, lines)
+	}
+
+	t.Log("Single line")
+	{
+		inputStr := `a single line`
+		//
+		lines := []string{}
+		err := WalkLines(inputStr, func(line string) error {
+			lines = append(lines, line)
+			return nil
+		})
+		require.Equal(t, nil, err)
+		require.Equal(t, []string{"a single line"}, lines)
+	}
+
+	t.Log("Two lines")
+	{
+		inputStr := `first line
+second line`
+		//
+		lines := []string{}
+		err := WalkLines(inputStr, func(line string) error {
+			lines = append(lines, line)
+			return nil
+		})
+		require.Equal(t, nil, err)
+		require.Equal(t, []string{"first line", "second line"}, lines)
+	}
+
+	t.Log("Multi line, with long line")
+	{
+		inputStr := fmt.Sprintf(`first line
+second line
+third, really long line: %s
+  fourth line
+`, strings.Repeat("-", 1000000))
+
+		//
+		lines := []string{}
+		err := WalkLines(inputStr, func(line string) error {
+			lines = append(lines, line)
+			return nil
+		})
+		require.Equal(t, nil, err)
+		//
+		require.Equal(t, 4, len(lines))
+		require.Equal(t, "first line", lines[0])
+		require.Equal(t, "second line", lines[1])
+		// check the start of the long line
+		require.Equal(t, "third, really long line: ---", lines[2][0:28])
+		require.Equal(t, "  fourth line", lines[3])
+	}
+
+	t.Log("Break early, with io.EOF")
+	{
+		inputStr := `first line
+second line
+break here
+this should not be included`
+		//
+		lines := []string{}
+		err := WalkLines(inputStr, func(line string) error {
+			if line == "break here" {
+				return io.EOF
+			}
+			lines = append(lines, line)
+			return nil
+		})
+		require.Equal(t, nil, err)
+		require.Equal(t, []string{"first line", "second line"}, lines)
+	}
+
+	t.Log("Break early, with a non io.EOF error")
+	{
+		inputStr := `first line
+second line
+break here
+this should not be included`
+		//
+		lines := []string{}
+		err := WalkLines(inputStr, func(line string) error {
+			if line == "break here" {
+				return errors.New("Please stop!")
+			}
+			lines = append(lines, line)
+			return nil
+		})
+		require.EqualError(t, err, "Please stop!")
+		require.Equal(t, []string{"first line", "second line"}, lines)
 	}
 }

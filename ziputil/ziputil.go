0ad4eb40d96a049c65c5ca8be205b3092f6a9d55
Krisztian Godrei <godreikrisztian@gmail.com>
2017-05-26 11:27:16 +0200

    zip content of dir, unzip fix

--- ziputil/ziputil.go
+++ ziputil/ziputil.go
@@ -2,7 +2,6 @@ package ziputil
 
 import (
 	"fmt"
-	"os"
 	"path/filepath"
 
 	"github.com/bitrise-io/go-utils/command"
@@ -10,57 +9,50 @@ import (
 )
 
 // Zip ...
-func Zip(sourcePth, destinationZipPth string) error {
-	parentDir := filepath.Dir(sourcePth)
-	dirName := filepath.Base(sourcePth)
-	cmd := command.New("/usr/bin/zip", "-rTy", destinationZipPth, dirName)
-	cmd.SetDir(parentDir)
+func Zip(sourcePth, destinationZipPth string, isContentOnly bool) error {
+	workDir := ""
+	isDir := false
+
+	if isContentOnly {
+		if exist, err := pathutil.IsDirExists(sourcePth); err != nil {
+			return err
+		} else if exist {
+			workDir = sourcePth
+			isDir = true
+		}
+	}
+
+	if workDir == "" {
+		workDir = filepath.Dir(sourcePth)
+	}
+
+	targetName := ""
+	if isContentOnly && isDir {
+		targetName = "."
+	} else {
+		targetName = filepath.Base(sourcePth)
+	}
+
+	cmd := command.New("/usr/bin/zip", "-rTy", destinationZipPth, targetName)
+	cmd.SetDir(workDir)
 	out, err := cmd.RunAndReturnTrimmedCombinedOutput()
 	if err != nil {
-		return fmt.Errorf("Failed to zip: %s, output: %s, error: %s", sourcePth, out, err)
+		return fmt.Errorf("$ %s\nFailed - output: %s, error: %s", cmd.PrintableCommandArgs(), out, err)
 	}
+
 	return nil
 }
 
 // UnZip ...
-func UnZip(sourceZipPth string) (string, error) {
-	// copy zip to tmp dir
-	tmpDir, err := pathutil.NormalizedOSTempDirPath("__unzip__")
-	if err != nil {
-		return "", err
-	}
+func UnZip(zip, intoDir string) error {
+	workDir := filepath.Dir(intoDir)
 
-	tmpSourcePth := filepath.Join(tmpDir, filepath.Base(sourceZipPth))
-	if err := command.CopyFile(sourceZipPth, tmpSourcePth); err != nil {
-		return "", err
-	}
-	// ---
-
-	// unzip
-	cmd := command.New("/usr/bin/unzip", sourceZipPth)
-	cmd.SetDir(tmpDir)
+	cmd := command.New("/usr/bin/unzip", zip, "-d", intoDir)
+	cmd.SetDir(workDir)
 	out, err := cmd.RunAndReturnTrimmedCombinedOutput()
 	if err != nil {
-		return "", fmt.Errorf("Failed to unzip: %s, output: %s, error: %s", tmpSourcePth, out, err)
-	}
-	// ---
-
-	// serch for the unzipped path
-	if err := os.RemoveAll(tmpSourcePth); err != nil {
-		return "", err
+		return fmt.Errorf("$ %s\nFailed - output: %s, error: %s", cmd.PrintableCommandArgs(), out, err)
 	}
 
-	pattern := filepath.Join(tmpDir, "*")
-	pths, err := filepath.Glob(pattern)
-	if err != nil {
-		return "", err
-	}
-	if len(pths) == 0 {
-		return "", fmt.Errorf("unzipped file not found")
-	} else if len(pths) > 1 {
-		return "", fmt.Errorf("multiple file generated")
-	}
-	// ---
-
-	return pths[0], nil
+	return nil
 }

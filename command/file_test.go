caf2054915f47acd679074eeaa7565a140e2bc0d
Tamas Kadar <ktamas@ktamas.com>
2017-10-11 16:06:03 +0200

    Updated test

--- command/file_test.go
+++ command/file_test.go
@@ -1,20 +1,17 @@
 package command
 
 import (
-	"os/exec"
 	"testing"
 
+	"github.com/bitrise-io/go-utils/pathutil"
 	"github.com/stretchr/testify/require"
 )
 
 func TestCopyFileErrorIfDirectory(t *testing.T) {
 	t.Log("It fails if source is a directory")
 	{
-		cmd := exec.Command("mkdir", "source_dir")
-		cmd.Run()
-		err := CopyFile("source_dir", "./nothing/whatever")
-		require.Error(t, err)
-		cmd = exec.Command("rm", "-rf", "./source_dir/")
-		cmd.Run()
+		tmpFolder, err := pathutil.NormalizedOSTempDirPath("_tmp")
+		require.NoError(t, err)
+		require.EqualError(t, CopyFile(tmpFolder, "./nothing/whatever"), "Source is a directory: "+tmpFolder)
 	}
 }

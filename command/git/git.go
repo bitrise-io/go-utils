ff92b821670ba152238c830b36a56f249eac03a2
Krisztián Gödrei <godreikrisztian@gmail.com>
2017-12-08 11:00:20 +0100

    pr fix

--- command/git/git.go
+++ command/git/git.go
@@ -13,7 +13,7 @@ type Git struct {
 
 // New creates a new git project.
 func New(dir string) (Git, error) {
-	if err := os.MkdirAll(dir, 0777); err != nil {
+	if err := os.MkdirAll(dir, 0755); err != nil {
 		return Git{}, err
 	}
 	return Git{dir: dir}, nil

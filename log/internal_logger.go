ba5d59964f75a2fa3783fba989424ce02dee390a
Laszlo Szucs <laszlo.szucs@bitrise.io>
2019-04-02 12:41:56 +0200

    fix: adhere to enable debug log config

--- log/internal_logger.go
+++ log/internal_logger.go
@@ -25,25 +25,27 @@ func init() {
 
 // SendToInternal sends the log message to the configured analytics server
 func (lm Message) SendToInternal(stepID, tag string, data map[string]interface{}) {
-	for k, v := range data {
-		lm.Data[k] = v
-	}
-
-	lm.Data["step_id"] = stepID
-	lm.Data["tag"] = tag
-
-	b, err := json.Marshal(lm)
-	if err != nil {
-		fmt.Printf("marshal log message: %s\n", err)
-	}
-
-	resp, err := http.Post(analyticsServerURL + "/logs", "application/json", bytes.NewReader(b))
-	if err != nil {
-		fmt.Printf("post log message: %s\n", err)
-	}
-
-	if resp.StatusCode != 200 {
-		fmt.Printf("post log message response: %s %s\n", resp.Status, resp.Body)
+	if enableDebugLog {
+		for k, v := range data {
+			lm.Data[k] = v
+		}
+	
+		lm.Data["step_id"] = stepID
+		lm.Data["tag"] = tag
+	
+		b, err := json.Marshal(lm)
+		if err != nil {
+			fmt.Printf("marshal log message: %s\n", err)
+		}
+	
+		resp, err := http.Post(analyticsServerURL + "/logs", "application/json", bytes.NewReader(b))
+		if err != nil {
+			fmt.Printf("post log message: %s\n", err)
+		}
+	
+		if resp.StatusCode != 200 {
+			fmt.Printf("post log message response: %s %s\n", resp.Status, resp.Body)
+		}	
 	}
 }
 

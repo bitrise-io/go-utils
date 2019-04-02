f9bc4952ee2e50e5b8f982f63114baa95ed75282
Laszlo Szucs <laszlo.szucs@bitrise.io>
2019-04-02 11:28:17 +0200

    wip: send log message to analytics server

--- log/internal_logger.go
+++ log/internal_logger.go
@@ -1,5 +1,12 @@
 package log
 
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
 type logMessage struct{
 	LogLevel string `json:"log_level"`
 	Message string `json:"message"`
@@ -7,6 +14,25 @@ type logMessage struct{
 }
 
 func (lm logMessage) SendToInternal(stepID, tag string, data map[string]interface{}) {
-	// todo: post to analytics server
+	for k, v := range data {
+		lm.Data[k] = v
+	}
+
+	lm.Data["step_id"] = stepID
+	lm.Data["tag"] = tag
+
+	b, err := json.Marshal(lm)
+	if err != nil {
+		fmt.Printf("marshal log message: %s\n", err)
+	}
+
+	resp, err := http.Post("https://bitrise-step-analytics.herokuapp.com/logs", "application/json", bytes.NewReader(b))
+	if err != nil {
+		fmt.Printf("post log message: %s\n", err)
+	}
+
+	if resp.StatusCode != 200 {
+		fmt.Printf("post log message response: %s %s\n", resp.Status, resp.Body)
+	}
 }
 

e99d738ea1c6f0417ce958b77368f57d5f3bff64
Laszlo Pusok <laszlo.pusok@bitrise.io>
2019-06-21 00:00:14 +0200

    Adding bundler related commands

---
+++ command/bundler/bundler.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"github.com/bitrise-io/go-utils/command"
+	"github.com/bitrise-io/go-utils/command/rubycommand"
+)
+
+// InstallBundlerCommand returns a command to install a specific bundler version
+func InstallBundlerCommand(gemfileLockVersion GemVersion) (*command.Model, error) {
+	installBundlerCmdParams := []string{"gem", "install", "bundler", "--force", "--no-document"}
+	if gemfileLockVersion.found {
+		installBundlerCmdParams = append(installBundlerCmdParams, []string{"-v", gemfileLockVersion.version}...)
+	}
+
+	return command.NewFromSlice(installBundlerCmdParams)
+}
+
+// BundleInstallCommand returns a command to install a bundle using bundler
+func BundleInstallCommand(gemfileLockVersion GemVersion) (*command.Model, error) {
+	bundleInstallCmdParams := []string{"bundle"}
+	if gemfileLockVersion.found {
+		bundleInstallCmdParams = append(bundleInstallCmdParams, "_"+gemfileLockVersion.version+"_")
+	}
+	bundleInstallCmdParams = append(bundleInstallCmdParams, []string{"install", "--jobs", "20", "--retry", "5"}...)
+
+	return rubycommand.NewFromSlice(bundleInstallCmdParams)
+}
+
+// RbenvVersionsCommand retruns a command to print used and available ruby versions if rbenv is installed
+func RbenvVersionsCommand() (*command.Model, error) {
+	if _, err := command.New("which", "rbenv").RunAndReturnTrimmedCombinedOutput(); err != nil {
+		return nil, err
+	}
+
+	return command.New("rbenv", "versions"), nil
+}
---
+++ command/bundler/gemfile_lock.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+	"strings"
+)
+
+// GemVersion contains bundler or fastlane version
+type GemVersion struct {
+	version string
+	found   bool
+}
+
+// ParseFastlaneVersion  returns the fastlane version parsed from a Gemfile.lock on a best effort basis, for logging purposes only.
+//
+// for the following Gemfile.lock example, it returns: ">= 2.0)"
+//   specs:
+//     CFPropertyList (3.0.0)
+//     addressable (2.6.0)
+//       public_suffix (>= 2.0.2, < 4.0)
+//     atomos (0.1.3)
+//     babosa (1.0.2)
+//     badge (0.8.5)
+//       curb (~> 0.9)
+//       fastimage (>= 1.6)
+//       fastlane (>= 2.0)
+//       mini_magick (>= 4.5)
+//     claide (1.0.2)
+func ParseFastlaneVersion(gemfileLockContent string) (gemVersion GemVersion, err error) {
+	relevantLines := []string{}
+	lines := strings.Split(gemfileLockContent, "\n")
+
+	specsStart := false
+	for _, line := range lines {
+		if strings.Contains(line, "specs:") {
+			specsStart = true
+		}
+
+		trimmed := strings.Trim(line, " ")
+		if trimmed == "" {
+			specsStart = false
+		}
+
+		if specsStart {
+			relevantLines = append(relevantLines, line)
+		}
+	}
+
+	//     fastlane (1.109.0)
+	exp := regexp.MustCompile(fmt.Sprintf(`^%s \((.+)\)`, regexp.QuoteMeta("fastlane")))
+	for _, line := range relevantLines {
+		match := exp.FindStringSubmatch(strings.TrimSpace(line))
+		if match == nil {
+			continue
+		}
+		if len(match) != 2 {
+			return GemVersion{}, fmt.Errorf("unexpected regexp match: %v", match)
+		}
+		return GemVersion{
+			version: match[1],
+			found:   true,
+		}, nil
+	}
+
+	return GemVersion{}, nil
+}
+
+// ParseBundlerVersion returns the bundler version used to create the bundle
+func ParseBundlerVersion(gemfileLockContent string) (gemVersion GemVersion, err error) {
+	/*
+		BUNDLED WITH
+			1.17.1
+	*/
+	bundlerRegexp := regexp.MustCompile(`(?m)^BUNDLED WITH\n\s+(\S+)`)
+	match := bundlerRegexp.FindStringSubmatch(gemfileLockContent)
+	if match == nil {
+		return GemVersion{}, nil
+	}
+	if len(match) != 2 {
+		return GemVersion{}, fmt.Errorf("unexpected regexp match: %v", match)
+	}
+
+	return GemVersion{
+		version: match[1],
+		found:   true,
+	}, nil
+}

b32f8a49333bf8fcae235c1134bd8198c1052a19
Akos Birmacher <akos.birmacher@bitrise.io>
2019-11-06 13:34:03 +0100

    PR fix

--- command/rubycommand/rubycommand.go
+++ command/rubycommand/rubycommand.go
@@ -18,11 +18,6 @@ const (
 	brewRubyPthAlt = "/usr/local/opt/ruby/bin/ruby"
 )
 
-// RubyInstallType returns which version manager was used for the ruby install
-func RubyInstallType() InstallType {
-	return installType()
-}
-
 // InstallType ...
 type InstallType int8
 
@@ -52,7 +47,8 @@ func cmdExist(slice ...string) bool {
 	return (cmd.Run() == nil)
 }
 
-func installType() InstallType {
+// RubyInstallType returns which version manager was used for the ruby install
+func RubyInstallType() InstallType {
 	whichRuby, err := command.New("which", "ruby").RunAndReturnTrimmedCombinedOutput()
 	if err != nil {
 		return Unkown
@@ -109,7 +105,7 @@ func sudoNeeded(installType InstallType, slice ...string) bool {
 
 // NewWithParams ...
 func NewWithParams(params ...string) (*command.Model, error) {
-	rubyInstallType := installType()
+	rubyInstallType := RubyInstallType()
 	if rubyInstallType == Unkown {
 		return nil, errors.New("unknown ruby installation type")
 	}
@@ -143,7 +139,7 @@ func GemUpdate(gem string) ([]*command.Model, error) {
 
 	cmds = append(cmds, cmd)
 
-	rubyInstallType := installType()
+	rubyInstallType := RubyInstallType()
 	if rubyInstallType == RbenvRuby {
 		cmd, err := New("rbenv", "rehash")
 		if err != nil {
@@ -172,7 +168,7 @@ func GemInstall(gem, version string) ([]*command.Model, error) {
 
 	cmds = append(cmds, cmd)
 
-	rubyInstallType := installType()
+	rubyInstallType := RubyInstallType()
 	if rubyInstallType == RbenvRuby {
 		cmd, err := New("rbenv", "rehash")
 		if err != nil {

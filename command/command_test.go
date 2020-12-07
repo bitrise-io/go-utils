53fa38299ac65cb889b501fa9486e1353da3e597
Krisztián Gödrei <krisztian.godrei@bitrise.io>
2020-12-07 11:44:35 +0100

    Black box test cocommand package.

--- command/command_test.go
+++ command/command_test.go
@@ -1,28 +1,30 @@
-package command
+package command_test
 
 import (
+	"os/exec"
 	"testing"
 
+	"github.com/bitrise-io/go-utils/command"
 	"github.com/stretchr/testify/require"
 )
 
 func TestNewCommandSlice(t *testing.T) {
 	t.Log("it fails if slice empty")
 	{
-		cmd, err := NewFromSlice([]string{})
+		cmd, err := command.NewFromSlice([]string{})
 		require.Error(t, err)
-		require.Equal(t, (*Model)(nil), cmd)
+		require.Equal(t, (*command.Model)(nil), cmd)
 	}
 
 	t.Log("it creates cmd if cmdSlice has 1 element")
 	{
-		_, err := NewFromSlice([]string{"ls"})
+		_, err := command.NewFromSlice([]string{"ls"})
 		require.NoError(t, err)
 	}
 
 	t.Log("it creates cmd if cmdSlice has multiple elements")
 	{
-		_, err := NewFromSlice([]string{"ls", "-a", "-l", "-h"})
+		_, err := command.NewFromSlice([]string{"ls", "-a", "-l", "-h"})
 		require.NoError(t, err)
 	}
 }
@@ -30,20 +32,20 @@ func TestNewCommandSlice(t *testing.T) {
 func TestNewWithParams(t *testing.T) {
 	t.Log("it fails if params empty")
 	{
-		cmd, err := NewWithParams()
+		cmd, err := command.NewWithParams()
 		require.Error(t, err)
-		require.Equal(t, (*Model)(nil), cmd)
+		require.Equal(t, (*command.Model)(nil), cmd)
 	}
 
 	t.Log("it creates cmd if params has 1 element")
 	{
-		_, err := NewWithParams("ls")
+		_, err := command.NewWithParams("ls")
 		require.NoError(t, err)
 	}
 
 	t.Log("it creates cmd if params has multiple elements")
 	{
-		_, err := NewWithParams("ls", "-a", "-l", "-h")
+		_, err := command.NewWithParams("ls", "-a", "-l", "-h")
 		require.NoError(t, err)
 	}
 }

d3340c44caf74c6af7c8ca5dfa42ea7c8929310c
Krisztián Gödrei <krisztian.godrei@bitrise.io>
2020-12-07 12:55:02 +0100

    Improve RunCmdAndReturnExitCode.

--- command/command_test.go
+++ command/command_test.go
@@ -49,3 +49,52 @@ func TestNewWithParams(t *testing.T) {
 		require.NoError(t, err)
 	}
 }
+
+func TestRunCmdAndReturnExitCode(t *testing.T) {
+	type args struct {
+		cmd *exec.Cmd
+	}
+	tests := []struct {
+		name         string
+		args         args
+		wantExitCode int
+		wantErr      bool
+	}{
+		{
+			name: "env command",
+			args: args{
+				cmd: exec.Command("env"),
+			},
+			wantExitCode: 0,
+			wantErr:      false,
+		},
+		{
+			name: "not existing executable",
+			args: args{
+				cmd: exec.Command("bash", "testdata/not_existing_executable.sh"),
+			},
+			wantExitCode: 127,
+			wantErr:      true,
+		},
+		{
+			name: "exit 42",
+			args: args{
+				cmd: exec.Command("bash", "testdata/exit_42.sh"),
+			},
+			wantExitCode: 42,
+			wantErr:      true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotExitCode, err := command.RunCmdAndReturnExitCode(tt.args.cmd)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("RunCmdAndReturnExitCode() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if gotExitCode != tt.wantExitCode {
+				t.Errorf("RunCmdAndReturnExitCode() = %v, want %v", gotExitCode, tt.wantExitCode)
+			}
+		})
+	}
+}

094c2d590d0bafc129778c487360109c3c8e70f5
Krisztián Gödrei <krisztian.godrei@bitrise.io>
2020-12-10 09:21:53 +0100

    Convert errorutil test to blackbox tests.

--- errorutil/errorutil_test.go
+++ errorutil/errorutil_test.go
@@ -1,42 +1,43 @@
-package errorutil
+package errorutil_test
 
 import (
 	"os/exec"
 	"testing"
 
+	"github.com/bitrise-io/go-utils/errorutil"
 	"github.com/stretchr/testify/require"
 )
 
 func TestIsExitStatusErrorStr(t *testing.T) {
 	// --- Should match ---
-	require.Equal(t, true, IsExitStatusErrorStr("exit status 1"))
-	require.Equal(t, true, IsExitStatusErrorStr("exit status 0"))
-	require.Equal(t, true, IsExitStatusErrorStr("exit status 2"))
-	require.Equal(t, true, IsExitStatusErrorStr("exit status 11"))
-	require.Equal(t, true, IsExitStatusErrorStr("exit status 111"))
-	require.Equal(t, true, IsExitStatusErrorStr("exit status 999"))
+	require.Equal(t, true, errorutil.IsExitStatusErrorStr("exit status 1"))
+	require.Equal(t, true, errorutil.IsExitStatusErrorStr("exit status 0"))
+	require.Equal(t, true, errorutil.IsExitStatusErrorStr("exit status 2"))
+	require.Equal(t, true, errorutil.IsExitStatusErrorStr("exit status 11"))
+	require.Equal(t, true, errorutil.IsExitStatusErrorStr("exit status 111"))
+	require.Equal(t, true, errorutil.IsExitStatusErrorStr("exit status 999"))
 
 	// --- Should not match ---
-	require.Equal(t, false, IsExitStatusErrorStr("xit status 1"))
-	require.Equal(t, false, IsExitStatusErrorStr("status 1"))
-	require.Equal(t, false, IsExitStatusErrorStr("exit status "))
-	require.Equal(t, false, IsExitStatusErrorStr("exit status"))
-	require.Equal(t, false, IsExitStatusErrorStr("exit status 2112"))
-	require.Equal(t, false, IsExitStatusErrorStr("exit status 21121"))
+	require.Equal(t, false, errorutil.IsExitStatusErrorStr("xit status 1"))
+	require.Equal(t, false, errorutil.IsExitStatusErrorStr("status 1"))
+	require.Equal(t, false, errorutil.IsExitStatusErrorStr("exit status "))
+	require.Equal(t, false, errorutil.IsExitStatusErrorStr("exit status"))
+	require.Equal(t, false, errorutil.IsExitStatusErrorStr("exit status 2112"))
+	require.Equal(t, false, errorutil.IsExitStatusErrorStr("exit status 21121"))
 
 	// prefixed
-	require.Equal(t, false, IsExitStatusErrorStr(".exit status 1"))
-	require.Equal(t, false, IsExitStatusErrorStr(" exit status 1"))
-	require.Equal(t, false, IsExitStatusErrorStr("error: exit status 1"))
+	require.Equal(t, false, errorutil.IsExitStatusErrorStr(".exit status 1"))
+	require.Equal(t, false, errorutil.IsExitStatusErrorStr(" exit status 1"))
+	require.Equal(t, false, errorutil.IsExitStatusErrorStr("error: exit status 1"))
 	// postfixed
-	require.Equal(t, false, IsExitStatusErrorStr("exit status 1."))
-	require.Equal(t, false, IsExitStatusErrorStr("exit status 1 "))
-	require.Equal(t, false, IsExitStatusErrorStr("exit status 1 - something else"))
-	require.Equal(t, false, IsExitStatusErrorStr("exit status 1 2"))
+	require.Equal(t, false, errorutil.IsExitStatusErrorStr("exit status 1."))
+	require.Equal(t, false, errorutil.IsExitStatusErrorStr("exit status 1 "))
+	require.Equal(t, false, errorutil.IsExitStatusErrorStr("exit status 1 - something else"))
+	require.Equal(t, false, errorutil.IsExitStatusErrorStr("exit status 1 2"))
 
 	// other
-	require.Equal(t, false, IsExitStatusErrorStr("-exit status 211-"))
-	require.Equal(t, false, IsExitStatusErrorStr("something else: exit status 1"))
+	require.Equal(t, false, errorutil.IsExitStatusErrorStr("-exit status 211-"))
+	require.Equal(t, false, errorutil.IsExitStatusErrorStr("something else: exit status 1"))
 }
 
 func TestCmdExitCodeFromError(t *testing.T) {
@@ -72,7 +73,7 @@ func TestCmdExitCodeFromError(t *testing.T) {
 			err := tt.cmd.Run()
 
 			// Act
-			got, err := CmdExitCodeFromError(err)
+			got, err := errorutil.CmdExitCodeFromError(err)
 
 			// Assert
 			require.Equal(t, tt.want, got)
@@ -104,7 +105,7 @@ func TestIsExitStatusError(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got := IsExitStatusError(tt.err)
+			got := errorutil.IsExitStatusError(tt.err)
 			require.Equal(t, tt.want, got)
 		})
 	}

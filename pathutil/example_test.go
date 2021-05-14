4d1fb0330dfe474fbbbccc0fe886a096be8d9395
Krisztián Gödrei <krisztian.godrei@bitrise.io>
2021-05-14 10:34:30 +0200

    Move FilterFuncs to pathutil and update Example.

--- pathutil/example_test.go
+++ pathutil/example_test.go
@@ -2,17 +2,16 @@ package pathutil_test
 
 import (
 	"fmt"
-	"path/filepath"
 	"strings"
 
 	"github.com/bitrise-io/go-utils/pathutil"
 )
 
 func Example() {
-	apkFilter := func(s string) (bool, error) {
-		return filepath.Ext(s) == ".apk", nil
-	}
-	pths, err := pathutil.FilterPaths([]string{"/app2.apk", "/MyApp.ipa", "/app1.apk"}, apkFilter)
+	pths, err := pathutil.FilterPaths(
+		[]string{"/app2.apk", "/MyApp.ipa", "/app1.apk"},
+		pathutil.ExtensionFilter(".apk", true),
+	)
 	if err != nil {
 		panic(err)
 	}

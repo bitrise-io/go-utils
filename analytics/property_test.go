3a46400ab62baee91ae5d8f021c37b4e98fdfdd9
Mate Herber <mate.herber@bitrise.io>
2022-04-14 17:26:18 +0200

    Helper method to transform a json annotated struct to Properties (#158)

---
+++ analytics/property_test.go
@@ -0,0 +1,81 @@
+package analytics
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type simpleTestStruct struct {
+	Name             string `json:"name"`
+	Address          string `json:"address,omitempty"`
+	Age              int    `json:"age"`
+	Happy            bool   `json:"happy"`
+	NotJSONField     string
+	IgnoredJSONField string `json:"-"`
+}
+
+type complexTestStruct struct {
+	Name            string          `json:"name"`
+	Address         string          `json:"address,omitempty"`
+	City            string          `json:"city"`
+	Age             int             `json:"age"`
+	Happy           bool            `json:"happy"`
+	Cars            []string        `json:"cars"`
+	ColorCollection map[string]bool `json:"color_collection"`
+}
+
+type emptyTestStruct struct {
+}
+
+func TestNewProperty(t *testing.T) {
+	tests := []struct {
+		name string
+		item interface{}
+		want Properties
+	}{
+		{
+			name: "Simple Test",
+			item: simpleTestStruct{
+				Name:         "John",
+				Age:          42,
+				Happy:        true,
+				NotJSONField: "Not a json field",
+			},
+			want: Properties{
+				"name":         "John",
+				"age":          42,
+				"happy":        true,
+				"NotJSONField": "Not a json field",
+			},
+		},
+		{
+			name: "Complex Test",
+			item: complexTestStruct{
+				Name:            "John",
+				Age:             42,
+				Happy:           true,
+				Cars:            []string{"Ford", "Chevy"},
+				ColorCollection: map[string]bool{"red": true, "blue": false},
+			},
+			want: Properties{
+				"name":             "John",
+				"city":             "",
+				"age":              42,
+				"happy":            true,
+				"cars":             []string{"Ford", "Chevy"},
+				"color_collection": map[string]bool{"red": true, "blue": false},
+			},
+		},
+		{
+			name: "Empty Test",
+			item: emptyTestStruct{},
+			want: Properties{},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equalf(t, tt.want, NewProperty(tt.item), "NewProperty(%v)", tt.item)
+		})
+	}
+}
--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.16
 require (
 	github.com/bitrise-io/go-utils v1.0.1
 	github.com/gofrs/uuid v4.2.0+incompatible
+	github.com/mitchellh/mapstructure v1.4.3
 	github.com/stretchr/testify v1.7.0
 )

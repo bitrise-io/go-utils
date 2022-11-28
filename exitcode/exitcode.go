a8a2729338204a2c1deaba60d31564e05b782bb7
Isaac Halvorson <hello@hisaac.net>
2022-11-28 14:53:23 -0600

    Add ExitCode type

---
+++ exitcode/exitcode.go
@@ -0,0 +1,14 @@
+//go:generate stringer -type=ExitCode
+
+package exitcode
+
+// ExitCode is a simple integer type that represents an exit code. It can be used to provide a more semantically meaningful exit code than a simple integer.
+type ExitCode int
+
+const (
+	// Success indicates that the program exited successfully.
+	Success ExitCode = 0
+
+	// Fail indicates that the program exited unsuccessfully.
+	Fail ExitCode = 1
+)

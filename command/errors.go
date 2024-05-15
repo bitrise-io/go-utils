70689cc5933a7c10cdc83eba15744c32bf1c923e
LaszloP <7979773+lpusok@users.noreply.github.com>
2024-05-15 20:11:52 +0200

    Simplify New

--- command/errors.go
+++ command/errors.go
@@ -9,20 +9,24 @@ import (
 
 // ExitStatusError ...
 type ExitStatusError struct {
-	readableReason     error
-	originalCommandErr error
+	readableReason  error
+	originalExitErr error
 }
 
 // NewExitStatusError ...
 func NewExitStatusError(printableCmdArgs string, exitErr *exec.ExitError, errorLines []string) error {
 	reasonMsg := fmt.Sprintf("command failed with exit status %d (%s)", exitErr.ExitCode(), printableCmdArgs)
 	if len(errorLines) == 0 {
-		reasonErr := fmt.Errorf("%s: %w", reasonMsg, errors.New("check the command's output for details"))
-		return &ExitStatusError{readableReason: reasonErr, originalCommandErr: exitErr}
+		return &ExitStatusError{
+			readableReason:  fmt.Errorf("%s: %w", reasonMsg, errors.New("check the command's output for details")),
+			originalExitErr: exitErr,
+		}
 	}
 
-	reasonErr := fmt.Errorf("%s: %w", reasonMsg, errors.New(strings.Join(errorLines, "\n")))
-	return &ExitStatusError{readableReason: reasonErr, originalCommandErr: exitErr}
+	return &ExitStatusError{
+		readableReason:  fmt.Errorf("%s: %w", reasonMsg, errors.New(strings.Join(errorLines, "\n"))),
+		originalExitErr: exitErr,
+	}
 }
 
 // Error returns the formatted error message. Does not include the original error message (`exit status 1`).
@@ -32,7 +36,7 @@ func (e *ExitStatusError) Error() string {
 
 // Unwrap is needed for errors.Is and errors.As to work correctly.
 func (e *ExitStatusError) Unwrap() error {
-	return e.originalCommandErr
+	return e.originalExitErr
 }
 
 // Reason returns the user-friendly error, to be used by errorutil.ErrorFormatter.

6c33f105d805be6c252bd9fd02b7b3b1634f8d57
Krisztián Gödrei <krisztian.godrei@bitrise.io>
2026-07-03 15:27:07 +0200

    ziputil: gofmt os_proxy.go comment alignment
    The RealOS method comments were hand-padded and gofmt-dirty; run gofmt.

--- ziputil/os_proxy.go
+++ ziputil/os_proxy.go
@@ -22,15 +22,15 @@ type OsProxy interface {
 // RealOS is the default implementation that delegates to the real os package.
 type RealOS struct{}
 
-func (RealOS) Lstat(name string) (os.FileInfo, error)            { return os.Lstat(name) }                          //nolint:revive
-func (RealOS) Readlink(name string) (string, error)              { return os.Readlink(name) }                       //nolint:revive
-func (RealOS) Open(name string) (*os.File, error)                { return os.Open(name) }                           //nolint:revive
-func (RealOS) Create(name string) (*os.File, error)              { return os.Create(name) }                         //nolint:revive
-func (RealOS) MkdirAll(path string, perm os.FileMode) error      { return os.MkdirAll(path, perm) }                 //nolint:revive
-func (RealOS) Symlink(oldname, newname string) error             { return os.Symlink(oldname, newname) }            //nolint:revive
-func (RealOS) EvalSymlinks(path string) (string, error)          { return filepath.EvalSymlinks(path) }             //nolint:revive
-func (RealOS) Remove(name string) error                          { return os.Remove(name) }                         //nolint:revive
-func (RealOS) Rename(oldpath, newpath string) error              { return os.Rename(oldpath, newpath) }             //nolint:revive
-func (RealOS) OpenFile(name string, flag int, perm os.FileMode) (*os.File, error) {                                 //nolint:revive
+func (RealOS) Lstat(name string) (os.FileInfo, error)       { return os.Lstat(name) }               //nolint:revive
+func (RealOS) Readlink(name string) (string, error)         { return os.Readlink(name) }            //nolint:revive
+func (RealOS) Open(name string) (*os.File, error)           { return os.Open(name) }                //nolint:revive
+func (RealOS) Create(name string) (*os.File, error)         { return os.Create(name) }              //nolint:revive
+func (RealOS) MkdirAll(path string, perm os.FileMode) error { return os.MkdirAll(path, perm) }      //nolint:revive
+func (RealOS) Symlink(oldname, newname string) error        { return os.Symlink(oldname, newname) } //nolint:revive
+func (RealOS) EvalSymlinks(path string) (string, error)     { return filepath.EvalSymlinks(path) }  //nolint:revive
+func (RealOS) Remove(name string) error                     { return os.Remove(name) }              //nolint:revive
+func (RealOS) Rename(oldpath, newpath string) error         { return os.Rename(oldpath, newpath) }  //nolint:revive
+func (RealOS) OpenFile(name string, flag int, perm os.FileMode) (*os.File, error) { //nolint:revive
 	return os.OpenFile(name, flag, perm)
 }
